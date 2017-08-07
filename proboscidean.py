ef4d845f2851a5faa655c5d1f28796c6c27509c8
chr <chr@cybre.space>
2017-08-06 22:50:45 -0700

    still figuring things out

--- proboscidean.py
+++ proboscidean.py
@@ -1,11 +1,15 @@
 import os, sys, time, datetime, threading, configparser, inspect
 from mastodon import Mastodon
 
-def interval(period):
+def interval(seconds):
     def wrapper(f):
-        f.interval = period
+        f.interval = seconds
         return f
     return wrapper
+
+def reply(f):
+    f.reply = True
+    return f
 
 class Proboscidean:
     INITIALIZING = 0
@@ -22,6 +26,7 @@ class Proboscidean:
 
         self._alive = threading.Condition()
         self._threads = []
+        self._reply_funcs = []
 
         self.username = ""
         self.display_name = ""
@@ -32,6 +37,7 @@ class Proboscidean:
         self._log = open(self._logname, "a")
 
         self._use_common_cfg = use_common_cfg
+        self._cfgname = Proboscidean.CONFIG if self._use_common_cfg else self.name + '.cfg'
 
         self.init()
         self.load_from_cfg()
@@ -40,15 +46,28 @@ class Proboscidean:
 
     def load_from_cfg(self):
         cfg = configparser.ConfigParser()
-        if self._use_common_cfg: cfg.read('config.cfg')
-        else: cfg.read(self.name + '.cfg')
+        cfg.read(self._cfgname)
 
-        # if (self.name not in section):
-        #     self.log(None, "\"{0}\" section not found in config file, aborting.\n")
+        if (self.name not in cfg.sections()):
+            self.log(None, "\"{0}\" section not found in {1}, aborting.\n".format(self.name, self._cfgname))
 
-        #section = cfg[self.name]
-        #if (section == None
-        #setattr(self, cfg.
+        section = cfg[self.name]
+        for attr in section:
+            setattr(self, attr, section[attr])
+
+    def save_to_cfg(self):
+        cfg = configparser.ConfigParser()
+        cfg.read(self._cfgname)
+
+        if (self.name not in cfg.sections()):
+            self.log(None, "\"{0}\" section not found in {1}, aborting.\n".format(self.name, self._cfgname))
+
+        section = cfg[self.name]
+        for attr, value in self.__dict__.items():
+            if attr[0] != '_':
+                cfg[attr] = value
+        with open(self._cfgname, 'w') as cfgfile:
+            cfg.write(cfgfile)
 
     def startup(self):
         self._state = Proboscidean.STARTING
@@ -75,7 +94,18 @@ class Proboscidean:
                 t.start()
                 self._threads.append(t)
 
+            if hasattr(f, "reply"):
+                self._reply_funcs.append(f)
+
         self._state = Proboscidean.RUNNING
+
+    @interval(seconds=60)
+    def run_scheduled(self):
+        pass
+
+    @interval(seconds=30)
+    def check_mentions(self):
+        pass
 
     def shutdown(self):
         self._alive.acquire()
---
+++ roll.py
@@ -0,0 +1,17 @@
+from proboscidean import Proboscidean, reply
+
+class roll(Proboscidean):
+    def init(self):
+        pass
+    def start(self):
+        pass
+    def stop(self):
+        pass
+
+    @reply
+    def parse_dice(message, user):
+        pass
+
+    def roll(dice=1, sides=6, drop=0):
+        pass
+
