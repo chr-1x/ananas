49169af53ea6f3a44955420ae4f630a786e836b8
Noëlle Anthony <noelle.d.anthony@gmail.com>
2020-02-03 12:25:45 -0500

    Pass error message through
    The existing error message suggests that ananas is having trouble loading the bot module, which can cause some troubleshooting difficulty when it's actually a module the *bot itself* is calling that's causing the ModuleNotFoundError. This change reports the name of the missing module to the user, so that they can more easily solve the problem.

--- ananas/run.py
+++ ananas/run.py
@@ -43,7 +43,8 @@ def main():
             exec("from {0} import {1}; bots.append({1}('{2}', name='{3}', interactive={4}, verbose={5}))"
                     .format(module, botclass, args.config, bot, args.interactive, args.verbose))
         except ModuleNotFoundError as e:
-            print("{}: couldn't load module {}, skipping {}.".format(prog, module, bot))
+            print("{}: encountered the following error loading module {}, skipping {}.".format(prog, module, bot))
+            print("{}: the error was: {}".format(prog, e)
             continue
         except Exception as e:
             print("{}: fatal exception loading bot {}: {}\n{}".format(prog, bot, repr(e), traceback.format_exc()))
